10ee7478b049ccb4d6b8dd04c0f57adbab15f2f8
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-03 12:12:49 +0530

    feat: Add dynamic menu items

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 const pizzaData = [
   {
     name: 'Focaccia',
@@ -47,7 +48,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className='container'>
       <Header />
       <Menu />
       <Footer />
@@ -56,33 +57,51 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast Pizza React Co</h1>;
+  const style = {};
+  return (
+    <header className='header'>
+      <h1 style={style}>Fast Pizza React Co</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
+      <ul className='pizzas'>
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <li className='pizza'>
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
+      </div>
+    </li>
   );
 }
 
 function Footer() {
-  return (
-    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
-  );
-}
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
 
-function Pizza() {
   return (
-    <div>
-      <img src='pizzas/spinaci.jpg' alt='Pizza Spinaci' />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    <footer className='footer'>
+      {new Date().toLocaleTimeString()} We're currently open
+    </footer>
   );
 }
 
